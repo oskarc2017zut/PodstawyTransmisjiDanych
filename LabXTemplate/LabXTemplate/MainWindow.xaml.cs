d0b40dd743159535e55eaec17e000ab8615b4918
oskarc2017zut <30659078+oskarc2017zut@users.noreply.github.com>
2017-10-04 22:45:49 +0200

    oxy

--- LabXTemplate/LabXTemplate/MainWindow.xaml.cs
+++ LabXTemplate/LabXTemplate/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,23 +14,57 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 using MahApps.Metro.Controls;
+using OxyPlot;
 
 namespace LabXTemplate
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : MetroWindow
+    public partial class MainWindow : MetroWindow, INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        DispatcherTimer timer = new DispatcherTimer();
+        Random rnd = new Random();
+        double time = 0d;
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            Data.Add(new DataPoint(time++, rnd.NextDouble() * 10));
+        }
+
         public MainWindow()
         {
             InitializeComponent();
+            timer.Interval = TimeSpan.FromMilliseconds(10);
+            timer.Tick += Timer_Tick;
+            //timer.Start();
         }
 
+        public ObservableCollection<DataPoint> Data
+        {
+            get { return (ObservableCollection<DataPoint>)GetValue(DataProperty); }
+            set { SetValue(DataProperty, value); }
+        }
 
+        // Using a DependencyProperty as the backing store for Data.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty DataProperty =
+            DependencyProperty.Register("Data", typeof(ObservableCollection<DataPoint>), typeof(MainWindow), new PropertyMetadata(new ObservableCollection<DataPoint>()));
 
-
-
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Data.Add(new DataPoint(time++, rnd.NextDouble() * 10));
+        }
     }
 }
