f05578202a45940d101e06e1aabd996aac9a1c7b
oskarc2017zut <30659078+oskarc2017zut@users.noreply.github.com>
2017-11-19 11:19:47 +0100

    lab2

---
+++ LabXTemplate/LabXTemplate/dft.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OxyPlot;
+using System.Numerics;
+
+namespace LabXTemplate
+{
+    partial class Zadania
+    {
+        public List<Complex> calculateDFT(List<Complex> x)
+        {
+            List<Complex> output = new List<Complex>();
+
+            int n = x.Count;
+
+            for (int k = 0; k < n; k++)
+            {
+                Complex sum = 0;
+                for (int t = 0; t < n; t++)
+                {
+                    double angle = 2 * Math.PI * t * k / n;
+                    sum += x[t] * Complex.Exp(new Complex(0, -angle));
+                }
+                output.Add(sum);
+            }
+
+
+            return output;
+        }
+    }
+}
